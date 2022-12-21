1dff25c90b2abbce9400528b3f4c13e10f03c842
Yesic402 <yesic.goez@gmail.com>
2022-12-20 23:23:19 -0500

    corrijo

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const path = require("path");
 const publicPath = path.resolve(__dirname,"./public");
 app.use(express.static("public"));
 app.listen(process.env.PORT || 3000, function() {console.log("Servidor corriendo");});
-app.get("/", (_, res)=>{res.sendFile(__dirname + "/views/home.html")})
+app.get("/", (_, res)=>{res.sendFile(path.join(__dirname, "/views/home.html"))})
 
