277a842b88c828dc452bc720d2f857cf1fbd7efa
Yesic402 <yesic.goez@gmail.com>
2022-12-20 23:49:50 -0500

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const publicPath = path.join(__dirname, "/public");
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 app.listen(process.env.PORT || 3000, function() {console.log("Servidor corriendo");});
 app.get("/", (_, res)=>{res.sendFile(path.join(__dirname, "/views/index.html"))})
 
