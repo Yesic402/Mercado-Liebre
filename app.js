ec6b8b038a35824fc20094289718409392db6aa0
Yesic402 <yesic.goez@gmail.com>
2022-12-20 23:09:54 -0500

    cuanto mas

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const app = express();
 const path = require("path");
 const publicPath = path.resolve(__dirname,"./public");
 app.use(express.static("public"));
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
-  })
-  
-
-  app.get("/", (_, res)=>{res.sendFile(__dirname + "/views/home.html")})
+app.listen(process.env.PORT || 3000, function() {console.log("Servidor corriendo");});
+app.get("/", (_, res)=>{res.sendFile(__dirname + "/views/home.html")})
 
